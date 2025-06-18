299049d5e23b699a213c7997b46a0ded20724b1d
gobrianna <whoamibrianna@tuta.io>
2025-06-18 14:44:23 -0400

    docs: edit comments

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,6 @@ import {
 
 dotenv.config();
 
-// debugging
-console.log("AWS_REGION:", process.env.AWS_REGION);
-console.log(
-  "AWS_ACCESS_KEY_ID:",
-  process.env.AWS_ACCESS_KEY_ID ? "*****" : "Missing"
-);
-console.log(
-  "AWS_SECRET_ACCESS_KEY:",
-  process.env.AWS_SECRET_ACCESS_KEY ? "*****" : "Missing"
-);
-
 const app = express();
 const PORT = 3000;
 
